1f636fdcfa9926b1ec2ece766a7974b8d039b501
Billy Bob <billy@hotmail.com>
2012-10-12 10:18:35 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clump::Application.config.secret_token = '00efd8869f53c9240bb8598d42a73502a0421b97f7e69ae66280032ea4196439f0468dcbf3526070f675b689fa11c133d2bc582a318450bf4692105e4fd1668a'
